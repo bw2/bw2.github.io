f4c88bed64b4be386a350d83c373fe9b470bddd2
bw2 <ben.weisburd@gmail.com>
2023-01-31 16:02:01 -0500

    fixed scripts/generate_html.py

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -4,13 +4,10 @@ import markdown
 import os
 import re
 
-#%%
 
 with open("scripts/blog_post_template.html", "rt") as f:
     blog_post_template = jinja2.Template(f.read())
 
-
-#%%
 
 for markdown_file_path in glob.glob("*.md"):
     if markdown_file_path == "README.md":
@@ -20,11 +17,14 @@ for markdown_file_path in glob.glob("*.md"):
     output_prefix = re.sub(".md$", "", markdown_file_path)
     output_html_path = f"{output_prefix}.html"
     title = os.path.basename(output_prefix)
+
     with open(markdown_file_path, "rt") as f:
-        markdown_html = markdown.markdown(f.read())
+        markdown_file_contents = f.read()
 
+    markdown_html = markdown.markdown(markdown_file_contents)
+
+    output_html = blog_post_template.render(markdown_html=markdown_html, title=title)
     with open(output_html_path, "wt") as f:
-        f.write(blog_post_template.render(markdown_html=markdown_html, title=title))
-
+        f.write(output_html)
 
 #%%
